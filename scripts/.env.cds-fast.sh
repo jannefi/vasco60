4fe6734bb0c09e022d43cad5c44a3c5915550795
Janne Ahlberg <janne.ahlberg@gmail.com>
2026-03-05 16:27:04 +0200

    default envs

---
+++ scripts/.env.cds-fast.sh
@@ -0,0 +1,17 @@
+
+# Fast single-shot CDS, minimal backoff, no internal blocksize param
+export VASCO_CDS_MODE=single
+export VASCO_CDS_MAX_RETRIES=2
+export VASCO_CDS_BASE_BACKOFF=1.5
+export VASCO_CDS_BLOCKSIZE=omit
+export VASCO_CDS_INTER_CHUNK_DELAY=0
+export VASCO_CDS_JITTER=0
+export VASCO_CDS_PRECALL_SLEEP=0
+export VASCO_CDS_GAIA_TABLE="I/355/gaiadr3"
+export VASCO_CDS_PS1_TABLE="II/389/ps1_dr2"
+export VASCO_SPIKES_ENGINE=vectorized
+export VASCO_SPIKES_DEBUG=1
+export VASCO_DISABLE_USNOB=1
+export VASCO_DISABLE_PS1=1
+export VASCO_CIRCLE_ARCMIN=30
+
